a3cc494a9c4fd47588beab1c57f76522bd5344bb
Jane Kaczmarek <jane.kaczmarek@csiro.au>
2018-11-21 12:25:11 +1100

    added G. Hobbs as author

--- source/conf.py
+++ source/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = u'Parkes User Guide'
-copyright = u'2018, Stacy Mader, Jane Kaczmarek, John Sarkissian, Jimi Green'
-author = u'Stacy Mader, Jane Kaczmarek, John Sarkissian, Jimi Green'
+copyright = u'2018, Stacy Mader, Jane Kaczmarek, John Sarkissian, Jimi Green, George Hobbs'
+author = u'Stacy Mader, Jane Kaczmarek, John Sarkissian, Jimi Green, George Hobbs'
 
 # The short X.Y version
 version = u''
